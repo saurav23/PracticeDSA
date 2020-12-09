c2f228d5f574fceaec7ff5d706b711688d0874db
saurav23 <sauravtarun813@gmail.com>
2020-12-09 17:39:37 +0530

    Update Dec 9

--- LL-2/midpointofLL.cpp
+++ LL-2/midpointofLL.cpp
@@ -3,3 +3,55 @@
 using namespace std;
 #include "node1.cpp"
 
+// Mid Point of the linked List
+node *midPointOfLl(node *head)
+{
+    //base case
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    node *slow = head;
+    node *fast = head->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+// taking input
+node *takeInput()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+
+        cin >> data;
+    }
+    return head;
+}
+
+// main
+int main()
+{
+    node *head = takeInput();
+    cout << midPointOfLl(head)  ;
+}
