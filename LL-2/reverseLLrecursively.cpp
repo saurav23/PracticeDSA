fd1a6bc20ac8bd6087248ce48167ebed7fdacf31
saurav23 <sauravtarun813@gmail.com>
2020-12-12 18:33:22 +0530

    Update Dec 12

---
+++ LL-2/reverseLLrecursively.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <cstring>
+using namespace std;
+#include "node1.cpp"
+
+node* reverseLL(){
+    
+}
+
+
+// ---------------------------------------------------------
+// print
+void printnode(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// taking input
+node *takeInputBetter()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+
+        cin >> data;
+    }
+    return head;
+}
+// ---------------------------------------------------------
+
+
+// main
+int main(){
+
+}
