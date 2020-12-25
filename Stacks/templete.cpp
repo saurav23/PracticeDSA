71ee71f5dac9d571e119c25bf038d0536bc22a75
saurav23 <sauravtarun813@gmail.com>
2020-12-26 02:17:33 +0530

    Dec 26

--- Stacks/templete.cpp
+++ Stacks/templete.cpp
@@ -3,7 +3,44 @@
 using namespace std;
 #include "intro.cpp"
 
+template <typename t, typename v>
+class Pair
+{
+    t x;
+    v y;
+
+public:
+    void setX(t x)
+    {
+        this->x = x;
+    }
+
+    t getX()
+    {
+        return x;
+    }
+
+    void setY(v y)
+    {
+        this->y = y;
+    }
+
+    v getY()
+    {
+        return y;
+    }
+};
+
 // main
-int main(){
-    
+int main()
+{
+    Pair<Pair<int, int>, int> p1;
+    p1.setY(15);
+    Pair<int, int> p4;
+    p4.setX(5);
+    p4.setY(10);
+    p1.setX(p4);
+    cout << p1.getX().getX() << endl;
+    cout << p1.getX().getY() << endl;
+    cout << p1.getY() << endl;
 }
